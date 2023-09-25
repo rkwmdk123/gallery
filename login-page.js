4b5ea2da52cf7617a888f02c879c4357df25808b
rkwmdk123 <145948883+rkwmdk123@users.noreply.github.com>
2023-09-25 15:13:30 +0900

    Add files via upload

---
+++ login-page.js
@@ -0,0 +1,17 @@
+const loginForm = document.getElementById("login-form");
+const loginButton = document.getElementById("login-form-submit");
+const loginErrorMsg = document.getElementById("login-error-msg");
+
+loginButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    const username = loginForm.username.value;
+    const password = loginForm.password.value;
+
+    if (username === "이상구" && password === "1234", username === "user" && password === "1234") {
+        alert("You have successfully logged in.");
+        window.location.href = "갤러리.html";
+        
+    } else {
+        loginErrorMsg.style.opacity = 1;
+    }
+});
